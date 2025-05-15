fb5c3eb37b2a7e676b3485a2381a6cbac8a83bdd
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-15 14:09:17 -0300

    initial commit

---
+++ src/test/java/com/velocirent/VelociRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velocirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelociRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
