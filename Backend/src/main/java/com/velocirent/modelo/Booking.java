197a16d8c58b6238b8a335f1fe272b3e964c8d6d
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-28 12:17:12 -0300

    connection to bd

---
+++ Backend/src/main/java/com/velocirent/modelo/Booking.java
@@ -0,0 +1,69 @@
+package com.velocirent.modelo;
+
+import java.util.Date;
+
+public class Booking {
+    private int id;
+    private int userId;
+    private int bikeId;
+    private Date startTime;
+    private Date endTime;
+    private String status;
+
+    public Booking(int id, int userId, int bikeId, Date startTime, Date endTime, String status) {
+        this.id = id;
+        this.userId = userId;
+        this.bikeId = bikeId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBikeId() {
+        return bikeId;
+    }
+
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
